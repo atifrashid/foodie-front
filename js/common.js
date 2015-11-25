ec2b62e3d975b9a368854d18c967753113d764e3
atifrashid <atifrashid@hotmail.com>
2015-11-25 21:39:28 +0500

    250

--- js/common.js
+++ js/common.js
@@ -8,7 +8,29 @@ var BAlert = $(
 		'<div class="modal-body"></div>' +
 		'<div class="modal-footer"><button type="button" class="btn btn-primary" data-dismiss="modal">OK</button></div>'+
 	'</div></div></div>');
+	
 
+function ajaxError( jqXHR ){
+	var M = '';
+	if (jqXHR.status === 0) {
+		M = 'Unable to connect to the server';
+	} else if (jqXHR.status == 404) {
+		M = 'Requested page not found. [404]';
+	} else if (jqXHR.status == 500) {
+		M = 'Internal Server Error [500].';
+	} else if (exception === 'parsererror') {
+		M = 'Requested JSON parse failed.';
+	} else if (exception === 'timeout') {
+		M = 'Request Time out.';
+	} else if (exception === 'abort') {
+		M = 'Ajax request aborted.';
+	} else {
+		M = 'Uncaught Error ' + jqXHR.responseText;
+	}
+	BAlert.find('.modal-header h3').html('<i class="fa fa-exclamation-triangle"></i> Error');
+	BAlert.find('.modal-body').html('<p class="text-danger">'+M+'</p>');
+	BAlert.modal();	
+}
 
 var getUrlParameter = function getUrlParameter(sParam) {
     var sPageURL = decodeURIComponent(window.location.search.substring(1)),
@@ -24,3 +46,38 @@ var getUrlParameter = function getUrlParameter(sParam) {
         }
     }
 };
+
+function ratingCal(ratSum, ratCount){
+	var r = 0;
+	var r2 = '0.0'; 
+	if( ratCount > 0 ){
+		r = ratSum / ratCount;
+		r2 = r.toFixed(1); 
+		r = (Math.round(r * 2) / 2).toFixed(1);
+	}
+	var ret = {  
+   	starImg : 'images/'+r+'.gif',
+		ratingTxt : '('+r2+'/5)'
+	}
+   return(ret);
+}
+
+function empty(mixed_var) {
+
+  var undef, key, i, len;
+  var emptyValues = ['undefined', undef, null, false, 0, '', '0'];
+
+  for (i = 0, len = emptyValues.length; i < len; i++) {
+    if (mixed_var === emptyValues[i]) {
+      return true;
+    }
+  }
+
+  if (typeof mixed_var === 'object') {
+    for (key in mixed_var) {
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
